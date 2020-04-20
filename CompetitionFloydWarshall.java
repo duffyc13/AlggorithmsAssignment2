5a678c928e2616fa256ed13e98d42ca2e9669a18
duffyc13 <60395760+duffyc13@users.noreply.github.com>
2020-04-20 21:26:42 +0100

    finished

--- CompetitionFloydWarshall.java
+++ CompetitionFloydWarshall.java
@@ -3,7 +3,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Scanner;
 
 
 /*
@@ -36,22 +35,24 @@ public class CompetitionFloydWarshall {
 	public int WEIGHT = 2;
 	double[][] graph=null;
 
-	private void initGraph(int totalIntersections)
+	private void initGraph(int intersections)
 	{
-
-		this.graph = new double[totalIntersections][totalIntersections];
-		for (int i = 0; i < graph.length; i++)
+		if (intersections!= 0)
 		{
-
-			for (int j = 0; j < graph.length; j++)
+			this.graph = new double[intersections][intersections];
+			for (int i = 0; i < graph.length; i++)
 			{
-				if (i == j)
+
+				for (int j = 0; j < graph.length; j++)
 				{
-					graph[i][j] = 0;
-				} 
-				else
-				{
-					graph[i][j] = Double.MAX_VALUE;
+					if (i == j)
+					{
+						graph[i][j] = 0;
+					} 
+					else
+					{
+						graph[i][j] = Double.MAX_VALUE;
+					}
 				}
 			}
 		}
@@ -60,6 +61,25 @@ public class CompetitionFloydWarshall {
 	private void addEdge(int source, int destination, double weight)
 	{
 		graph[source][destination] = weight;
+	}
+	
+	public double[][] floydWarshall()
+	{
+		double[][] weights;
+		int n = this.graph.length;
+		weights = Arrays.copyOf(this.graph, n);
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				for (int k = 0; k < n; k++)
+				{
+					weights[j][k] = Math.min(weights[j][k], weights[j][i] + weights[i][k]);
+				}
+			}
+		}
+		return weights;
 	}
 
 	/**
@@ -83,7 +103,6 @@ public class CompetitionFloydWarshall {
 				{
 					String[] intersectionsData = interNumber.split(" ");
 					this.intersections = Integer.parseInt (intersectionsData[0]);
-					initGraph(this.intersections);
 				}
 				String streetsNumber = bufferedReader.readLine(); // read number of streets
 				if (streetsNumber != null) 
@@ -91,6 +110,8 @@ public class CompetitionFloydWarshall {
 					String[] streetsData = streetsNumber.split(" ");
 					this.streets = Integer.parseInt (streetsData[0]);
 				}
+
+				initGraph(this.intersections);
 
 				boolean endOfFile = false;
 				while(!endOfFile) // read all street connections and create API for weighted graph 
@@ -131,7 +152,59 @@ public class CompetitionFloydWarshall {
 			return -1;
 		if (this.speed1<50||this.speed2<50||this.speed3<50)
 			return -1;
-		return -1;
-	}
+		if (this.graph==null)
+			return -1;
+		double[][] table = floydWarshall();
+		double time =0.0;
+		int greatestDist=0;
+		int greatestP=0;
+		double distanceA;
+		double distanceB;
+		double distanceC;
+		for (int i = 0; i< table.length; i++)
+		{
+			for (int indexA = 0; indexA < table.length; indexA++)
+			{
+				distanceA = table[i][indexA];
+				if (distanceA == Double.MAX_VALUE || distanceA < 0)
+					return -1;
+				distanceA = distanceA/speed1; 
+				if (distanceA > time)
+				{  
+					time = distanceA;
+					greatestDist = i;
+					greatestP = indexA;
+				}  
+				for (int indexB = 0; indexB < table.length; indexB++)
+				{
+					distanceB = table[indexB][i];
+					if (distanceB == Double.MAX_VALUE || distanceB < 0)
+						return -1;
+					distanceB = distanceB / speed2;
+					if (distanceB > time)
+					{
+						time = distanceB;
+						greatestDist = i;
+						greatestP = indexB;
+					}
 
+					for (int indexC = 0; indexC < table.length; indexC++)
+					{
+						distanceC = table[indexC][i];
+						if (distanceC == Double.MAX_VALUE || distanceC < 0)
+							return -1;
+						distanceC = distanceC/speed3; 
+						if (distanceC > time)
+						{
+							time = distanceC;
+							greatestDist = i;
+							greatestP = indexC;
+						}
+					}
+				}
+			}
+		}
+		int result=(int)Math.ceil(time);
+		return result;
+	}
 }
--- CompetitionTests.java
+++ CompetitionTests.java
@@ -4,60 +4,71 @@ import org.junit.Test;
 
 public class CompetitionTests {
 
-    @Test
-    public void testDijkstraConstructor() {
-    	String a="tinyEWD.txt";
-        CompetitionDijkstra comp = new CompetitionDijkstra(a, 50, 60, 90);
-    }
-    
-    @Test
-    public void testNoIn()
-    {
-      CompetitionDijkstra compD = new CompetitionDijkstra(null, 50, 60, 90);
-      assertEquals("Time required", -1, compD.timeRequiredforCompetition());
-      CompetitionFloydWarshall compF = new CompetitionFloydWarshall(null, 50, 60, 90);
-      assertEquals("Time required", -1, compF.timeRequiredforCompetition());
-    }
-    
-    @Test
-    public void testWrongSpeed()
-    {
-      CompetitionDijkstra compD = new CompetitionDijkstra("tinyEWD.txt", -20, 60, 90);
-      assertEquals("Time required", -1, compD.timeRequiredforCompetition());
-      CompetitionDijkstra compD1 = new CompetitionDijkstra("tinyEWD.txt", 50, 60, 101);
-      assertEquals("Time required", -1, compD1.timeRequiredforCompetition());
-      CompetitionFloydWarshall compF = new CompetitionFloydWarshall(null, 50, 60, 90);
-      assertEquals("Time required", -1, compF.timeRequiredforCompetition());
-      CompetitionFloydWarshall compF1 = new CompetitionFloydWarshall(null, 50, 60, 90);
-      assertEquals("Time required", -1, compF1.timeRequiredforCompetition());
-    }
+	@Test
+	public void testDijkstraConstructor() {
+		String a="tinyEWD.txt";
+		CompetitionDijkstra comp = new CompetitionDijkstra(a, 50, 60, 90);
+	}
 
-    @Test
-    public void testInputs()
-    {
-      CompetitionDijkstra comp = new CompetitionDijkstra("input-K.txt", 51, 70, 88);
-      assertEquals("Time required", 314, comp.timeRequiredforCompetition());
-      System.out.println("input-K: Minimum time required: " + comp.timeRequiredforCompetition() + " seconds");
-      CompetitionDijkstra comp2 = new CompetitionDijkstra("input-I.txt", 72, 70, 65);
-      assertEquals("Time required", 185, comp2.timeRequiredforCompetition());
-      System.out.println("input-I: Minimum time required: " + comp2.timeRequiredforCompetition() + " seconds");
-      CompetitionDijkstra comp3 = new CompetitionDijkstra("input-B.txt", 60, 80, 50);
-      assertEquals("Time required", 10000, comp3.timeRequiredforCompetition());
-      System.out.println("input-I: Minimum time required: " + comp3.timeRequiredforCompetition() + " seconds");
-    }
-    
-    @Test
-    public void testWrongFile()
-    {
-      CompetitionDijkstra comp = new CompetitionDijkstra("ta.txt", -20, 60, 90);
-      assertEquals("Time required", -1, comp.timeRequiredforCompetition());
-      CompetitionFloydWarshall compF = new CompetitionFloydWarshall("ta.txt", 50, 60, 90);
-      assertEquals("Time required", -1, compF.timeRequiredforCompetition());
-    }
-    
-    @Test
-    public void testFWConstructor() {
-    	String a="tinyEWD.txt";
-        CompetitionFloydWarshall compF = new CompetitionFloydWarshall(a, 50, 60, 90);
-    }
+	@Test
+	public void testNoIn()
+	{
+		CompetitionDijkstra compD = new CompetitionDijkstra(null, 50, 60, 90);
+		assertEquals("Time required", -1, compD.timeRequiredforCompetition());
+		CompetitionFloydWarshall compF = new CompetitionFloydWarshall(null, 50, 60, 90);
+		assertEquals("Time required", -1, compF.timeRequiredforCompetition());
+	}
+
+	@Test
+	public void testWrongSpeed()
+	{
+		CompetitionDijkstra compD = new CompetitionDijkstra("tinyEWD.txt", -20, 60, 90);
+		assertEquals("Time required", -1, compD.timeRequiredforCompetition());
+		CompetitionDijkstra compD1 = new CompetitionDijkstra("tinyEWD.txt", 50, 60, 101);
+		assertEquals("Time required", -1, compD1.timeRequiredforCompetition());
+		CompetitionFloydWarshall compF = new CompetitionFloydWarshall(null, 50, 60, 90);
+		assertEquals("Time required", -1, compF.timeRequiredforCompetition());
+		CompetitionFloydWarshall compF1 = new CompetitionFloydWarshall(null, 50, 60, 101);
+		assertEquals("Time required", -1, compF1.timeRequiredforCompetition());
+	}
+
+	@Test
+	public void testInputs()
+	{
+		CompetitionDijkstra comp = new CompetitionDijkstra("input-K.txt", 51, 70, 88);
+		assertEquals("Time required", 314, comp.timeRequiredforCompetition());
+		System.out.println("input-K: Minimum time required: " + comp.timeRequiredforCompetition());
+		CompetitionDijkstra comp2 = new CompetitionDijkstra("input-I.txt", 72, 70, 65);
+		assertEquals("Time required", 185, comp2.timeRequiredforCompetition());
+		System.out.println("input-I: Minimum time required: " + comp2.timeRequiredforCompetition());
+		CompetitionDijkstra comp3 = new CompetitionDijkstra("input-B.txt", 60, 80, 50);
+		assertEquals("Time required", 10000, comp3.timeRequiredforCompetition());
+		System.out.println("input-B: Minimum time required: " + comp3.timeRequiredforCompetition());
+		
+		CompetitionFloydWarshall compF = new CompetitionFloydWarshall("input-K.txt", 51, 70, 88);
+		assertEquals("Time required", 314, compF.timeRequiredforCompetition());
+		System.out.println("input-K: Minimum time required: " + compF.timeRequiredforCompetition());
+		CompetitionFloydWarshall compF2 = new CompetitionFloydWarshall("input-I.txt", 72, 70, 65);
+		assertEquals("Time required", 185, compF2.timeRequiredforCompetition());
+		System.out.println("input-I: Minimum time required: " + compF2.timeRequiredforCompetition());
+		CompetitionFloydWarshall compF3 = new CompetitionFloydWarshall("input-B.txt", 60, 80, 50);
+		assertEquals("Time required", 10000, compF3.timeRequiredforCompetition());
+		System.out.println("input-B: Minimum time required: " + compF3.timeRequiredforCompetition());
+		
+	}
+
+	@Test
+	public void testWrongFile()
+	{
+		CompetitionDijkstra comp = new CompetitionDijkstra("ta.txt", -20, 60, 90);
+		assertEquals("Time required", -1, comp.timeRequiredforCompetition());
+		CompetitionFloydWarshall compF = new CompetitionFloydWarshall("ta.txt", 50, 60, 90);
+		assertEquals("Time required", -1, compF.timeRequiredforCompetition());
+	}
+
+	@Test
+	public void testFWConstructor() {
+		String a="tinyEWD.txt";
+		CompetitionFloydWarshall compF = new CompetitionFloydWarshall(a, 50, 60, 90);
+	}
 }
